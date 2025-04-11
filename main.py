e23eccc65330002b92c0d0970870f5bfd8c1df15
Sebastian Ramirez <ramvillseb@gmai.com>
2025-04-11 01:59:55 -0700

    "prog"

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import random
 
 
 def main():
+    i=0
     total = 0
-    numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    numbers = [0]*5
+    while i<5:
+        numbers[i]=random.randint(0,100)
+        i=i+1
+    for z in numbers:
+        total+=z
+        if total <100:
+    
+            continue
+        else:
+            total=total-z
+            break
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
